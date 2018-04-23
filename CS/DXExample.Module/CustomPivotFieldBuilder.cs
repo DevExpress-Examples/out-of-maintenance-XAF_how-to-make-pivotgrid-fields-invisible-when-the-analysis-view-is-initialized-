71a75aa84ccf5cc209044393a2448f501b68f92b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXExample.Module/CustomPivotFieldBuilder.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.ExpressApp.PivotChart;
+
+namespace DXExample.Module {
+    public class CustomPivotFieldBuilder : PivotGridFieldBuilder {
+        public CustomPivotFieldBuilder(IAnalysisControl owner) : base(owner) { }
+        protected override void AddFieldCore(string caption, string bindingPropertyName, DevExpress.ExpressApp.DC.IMemberInfo propertyDescriptor) {
+            base.AddFieldCore(caption, bindingPropertyName, propertyDescriptor);
+            Owner.Fields[bindingPropertyName].AreaIndex = -1;
+        }
+    }
+}
